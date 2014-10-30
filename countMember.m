9b4038da69ce903ceef06362a67547894d2cafd9
Alan <alan.vey12@imperial.ac.uk>
2014-10-30 18:49:35 +0000

    Moved global functions to local funtions

--- countMember.m
+++
@@ -1,8 +0,0 @@
-function [count] = countMember(elem, pos, examples)
-  semiCount = 0;
-  for (i = 1 : size(examples, 1))
-    if (examples(i,pos) == elem)
-      semiCount = semiCount + 1;
-    end
-  end
-  count = semiCount;
--- getNumExamples.m
+++
@@ -1,7 +0,0 @@
-function [count] = getNumExamples(isPositive, attributeValue, index, examples, binary_targets)
-  count = 0;
-  for (i = 1 : size(examples))
-    if (examples(i, index) == attributeValue && binary_targets(i) == isPositive)
-      count = count + 1;
-    end
-  end
