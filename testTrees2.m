89527ef63eb8eb941b04680da92e2a394b50b063
Octavian Tuchila <octavian@dyn1223-123.wlan.ic.ac.uk>
2014-11-01 22:08:09 +0000

    Debugged testTrees, now choosing longest branch

--- testTrees2.m
+++ testTrees2.m
@@ -1,12 +1,13 @@
 function [predictions] = testTrees2(trees, x2)
-  blength = zeroes(1, size(x2, 1));
-  %chooses the tree with the shortest branch
+  fprintf('The number of examples is: %d', size(x2, 1));
+  %chooses the tree with the longest branch
   predictions = ones(length(x2), 1);
   for (i = 1 : length(x2))
+    maxBranch = 0;
     for (j = 2 : length(trees))
       branch = inTree(x2(i,:), trees(j));
-      if(branch >= 0 && (branch < blenght(i) || blength(i) == 0))
-        blength(i) = branch;
+      if(branch > maxBranch)
+        maxBranch = branch;
         predictions(i) = j;
       end
     end
@@ -15,7 +16,7 @@ end
 
 function [level] = inTree(val, tree)
   tree = tree{1,1};
-  level = 0;
+  level = 1;
   while (size(tree.kids, 1) > 0)
     tree = tree.kids{1, val(tree.op) + 1};
     level = level + 1;
