7d017e30fa4ede957e395c40188923b3b23bf1ac
Robert Prust <robertprust@hotmail.com>
2014-11-02 15:12:35 +0000

    Improved testTrees2, so that it picks the tree with the longest branch in the case that none of them match.

--- testTrees2.m
+++ testTrees2.m
@@ -1,27 +1,34 @@
 function [predictions] = testTrees2(trees, x2)
-  fprintf('The number of examples is: %d', size(x2, 1));
   %chooses the tree with the longest branch
   predictions = ones(length(x2), 1);
   for (i = 1 : length(x2))
     maxBranch = 0;
-    for (j = 2 : length(trees))
-      branch = inTree(x2(i,:), trees(j));
-      if(branch > maxBranch)
-        maxBranch = branch;
+    found = false;
+    for (j = 1 : length(trees))
+      info = inTree(x2(i,:), trees(j));
+      if (info(2))
+        if (~found)
+          maxBranch = info(1);
+          predictions(i) = j;
+          found = true;
+        elseif (info(1) > maxBranch)
+          maxBranch = info(1);
+          predictions(i) = j;
+        end
+      elseif (~found && info(1) > maxBranch)
+        maxBranch = info(1);
         predictions(i) = j;
       end
     end
   end
 end
 
-function [level] = inTree(val, tree)
+function [info] = inTree(val, tree)
   tree = tree{1,1};
-  level = 1;
+  info = [0,0];
   while (size(tree.kids, 1) > 0)
     tree = tree.kids{1, val(tree.op) + 1};
-    level = level + 1;
+    info(1) = info(1) + 1;
   end
-  if(tree.class == 0)
-      level = -1;
-  end
+  info(2) = tree.class;
 end
