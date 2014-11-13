a2947aaeef234f6ab8641e84a59b2968218d8993
Robert Prust <robertprust@hotmail.com>
2014-11-13 17:53:36 +0000

    Fixed folder organisation.

--- Neural Networks/Gradient Descent (traingd)/testANN.m
+++
@@ -1,5 +0,0 @@
-function [predictions] = testANN(net, x2)
-  result = sim(net, x2);
-  predictions = NNout2labels(result);
-end
-
--- Neural Networks/Gradient Descent (traingd)/totalConfusion.m
+++
@@ -1,6 +0,0 @@
-function [confusion] = totalConfusion(folds)
-  confusion = zeros(6,6);
-  for (i = 1 : length(folds))
-    confusion = confusion + generateConfusion(folds{i,1}, folds{i,2});
-  end
-end
