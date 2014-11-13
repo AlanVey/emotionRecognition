bc98a3107298c8c26f0c6eaf57d1bbd6030ce056
Robert Prust <robertprust@hotmail.com>
2014-11-13 22:46:05 +0000

    Rewrote optParamsGD to use a sequential approach.

--- Neural Networks/Gradient Descent (traingd)/optParamsGD.m
+++ Neural Networks/Gradient Descent (traingd)/optParamsGD.m
@@ -1,20 +1,46 @@
 function [bestLayerSize, bestNumLayers, bestValPc, bestLr, bestF1] = optParamsGD(x, y)
   bestF1 = 0;
+  numLayers = 3;
+  valPc = 0.1;
+  lr = 0.05;
+  
   for (layerSize = 45 : 100)
-    for (numLayers = 1 : 5)
-      for (valPc = 0.05 : 0.01 : 0.20)
-        for (lr = 0.01 : 0.01 : 0.1)
-          result = nFoldCrossValidationGD(x, y, 10, layerSize, numLayers, valPc, lr);
-          stats = generateAllStats(result);
-          if (calcMeanF1(stats) > bestF1)
-            bestF1 = calcMeanF1(stats);
-            bestLayerSize = layerSize;
-            bestNumLayers = numLayers;
-            bestValPc = valPc;
-            bestLr = lr;
-          end
-        end
-      end
+    layerSize
+    result = nFoldCrossValidationGD(x, y, 10, layerSize, numLayers, valPc, lr);
+    stats = generateAllStats(result);
+    if (calcMeanF1(stats) > bestF1)
+      bestF1 = calcMeanF1(stats);
+      bestLayerSize = layerSize;
+    end
+  end
+  bestF1 = 0;
+  
+  for (numLayers = 1 : 5)
+    result = nFoldCrossValidationGD(x, y, 10, bestLayerSize, numLayers, valPc, lr);
+    stats = generateAllStats(result);
+    if (calcMeanF1(stats) > bestF1)
+      bestF1 = calcMeanF1(stats);
+      bestNumLayers = numLayers;
+    end
+  end
+  bestF1 = 0;
+  
+  for (valPc = 0.05 : 0.01 : 0.20)
+    result = nFoldCrossValidationGD(x, y, 10, bestLayerSize, bestNumLayers, valPc, lr);
+    stats = generateAllStats(result);
+    if (calcMeanF1(stats) > bestF1)
+      bestF1 = calcMeanF1(stats);
+      bestValPc = valPc;
+    end
+  end
+  bestF1 = 0;
+  
+  for (lr = 0.01 : 0.01 : 0.1)
+    result = nFoldCrossValidationGD(x, y, 10, bestLayerSize, bestNumLayers, bestValPc, lr);
+    stats = generateAllStats(result);
+    if (calcMeanF1(stats) > bestF1)
+      bestF1 = calcMeanF1(stats);
+      bestLr = lr;
     end
   end
 end
