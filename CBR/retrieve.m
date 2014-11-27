490d5402f099da781e2fc7cc65356a0a2866cbad
Robert Prust <robertprust@hotmail.com>
2014-11-27 20:47:25 +0000

    Finished (fixed) CBR implementation, now runs with its own nFCV function.

--- CBR/retrieve.m
+++ CBR/retrieve.m
@@ -2,37 +2,45 @@ function [Case] = retrieve(CBR, newCase)
   k = 4;
   simType = 1;
   
-  sims = [];
+  sims = repmat(struct('AU', [], 'solution', 0, 'typicality', 0, 'similarity', 0), k, 1);
+  maxSim = intmin;
+  maxSimIndex = 0;
   
-  for (i = 1 : length(CBR))
+  for (i = 1 : k)
+    curCase = CBR(i);
+    curCase.similarity = similarity(CBR(i), newCase, simType);
+    sims(i) = curCase;
+    if (curCase.similarity > maxSim)
+      maxSim = curCase.similarity;
+      maxSimIndex = i;
+    end
+  end
+  
+  for (i = k + 1 : length(CBR))
     caseSim = similarity(CBR(i), newCase, simType);
-    sims(caseSim, length(sims, caseSim) + 1) = CBR(i);
+    if (caseSim < maxSim)
+      newCase = CBR(i);
+      newCase.similarity = caseSim;
+      sims(maxSimIndex) = newCase;
+      [maxSim, maxSimIndex] = getMaxSimilarity(sims);
+    end
   end
   
-  topSims = [];
+  avgRes = mode([sims.solution].');
   
   for (i = 1 : k)
-    topSims = cat(2, topSims, sims(i));
-  end
-  
-  topSims = topSims(1:k);
-  
-  avgRes = mode([topSims.solution].');
-  
-  topSims = fliplr(topSims);
-  for (i = 1 : k)
-    if (topSims(i).solution == avgRes)
-      Case = topSims(i);
+    if (sims(i).solution == avgRes)
+      Case = sims(i);
     end
   end
 end
 
-function [simil, index] = getMinSimilarity(topSimils)
-  simil = topSimils(1).Simil;
+function [simil, index] = getMaxSimilarity(sims)
+  simil = sims(1).similarity;
   index = 1;
-  for (i = 2 : length(topSimils))
-    if (topSimils(i).Simil < simil)
-      simil = topSimils(i).Simil;
+  for (i = 2 : length(sims))
+    if (sims(i).similarity > simil)
+      simil = sims(i).similarity;
       index = i;
     end
   end
